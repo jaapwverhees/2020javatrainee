4410547a876ae621f39598dd81ffb5431271d4e5
jaapwverhees <34657795+jaapwverhees@users.noreply.github.com>
2020-03-18 09:40:51 +0100

    Bees

--- src/main/java/human/Android.java
+++ src/main/java/human/Android.java
@@ -1,18 +1,32 @@
 package human;
 
-public class Android extends Human implements ChargeAble {
+public class Android extends Person implements ChargeAble {
 
-    private int Charge;
+    private int charge;
+
+    public Android(int charge) {
+        this.charge = charge;
+    }
+
+    public Android(String name, int age, int charge) {
+        super(name, age);
+        this.charge = charge;
+    }
+
+    public Android(String name, int age, Gender gender, int charge) {
+        super(name, age, gender);
+        this.charge = charge;
+    }
 
     @Override
     public String greeting() {
-        return String.format("I'm half human, my Energy Level is %s", this.Charge);
+        return String.format("I'm half human, my Energy Level is %s %s", this.charge, "%");
     }
 
     @Override
     public int charge(int amount) {
-        Charge = Charge + amount;
-        if(Charge > 100) Charge = 100;
-        return this.Charge;
+        charge = charge + amount;
+        if(charge > 100) charge = 100;
+        return this.charge;
     }
 }
